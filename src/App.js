f307eda854d4ec2295b276809831487ffadc00f1
Alex <byczko2012@mail.ru>
2021-10-22 16:32:12 +0300

    fix all APP

--- src/App.js
+++ src/App.js
@@ -1,94 +1,131 @@
-import React, {useEffect, useState} from 'react';
-import './App.scss';
-import Currency from "./Components/currency/Currency";
-import axios from "axios";
+import React, {Component} from "react";
 
-const BASE_URL = 'https://www.cbr-xml-daily.ru/latest.js'
+class Converter extends Component {
 
-function App() {
 
-    const [currencyName, setCurrencyName] = useState([])
-    const [fromCurrency, setFromCurrency] = useState([])
-    const [toCurrency, setToCurrency] = useState([])
-    const [exchangeRate, setExchangeRate] = useState([])
-    const [sum, setSum] = useState()
-    const [sumInFromCurrency, setSumInFromCurrency] = useState(true)
-
-    console.log(sumInFromCurrency)
-
-    let toSum, fromSum
-    if (sumInFromCurrency) {
-        debugger
-        fromSum = sum
-        toSum = sum * exchangeRate
-    } else {
-        debugger
-        toSum = sum
-        fromSum = sum / exchangeRate
-    }
-
-    useEffect(() => {
-        fetch(BASE_URL)
-            .then(res => res.json())
-            .then(data => {
-                const firstCurrency = Object.keys(data.rates)[0]
-                setCurrencyName([data.base, ...Object.keys(data.rates)])
-                setFromCurrency(data.base)
-                setToCurrency(firstCurrency)
-                setExchangeRate(data.rates[firstCurrency])
-            })
-    }, []);
-
-    useEffect(() => {
-        if (fromCurrency != null && toCurrency != null) {
-            fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
-                .then(response => response.json())
-                .then(data => setExchangeRate(data.rates[toCurrency]))
+    componentDidMount() {
+        const amount = this.state.amount;
+        if (amount === isNaN) {
+            return;
+        } else {
+            fetch(`https://www.cbr-xml-daily.ru/latest.js?base=${this.state.base}`)
+                .then(res => res.json())
+                .then(data => {
+                    const currencies = data.rates
+                    //const result = (currencies[this.state.convertTo] * amount);
+                    console.log(data)
+                    this.setState({
+                        currencies,
+                    });
+                });
         }
-    }, [fromCurrency, toCurrency])
-
-    function handleFromSumChange(e) {
-        setSum(e.target.value)
-        setSumInFromCurrency(true)
     }
 
-    function handleToSumChange(e) {
-        setSum(e.target.value)
-        setSumInFromCurrency(false)
-    }
+    state = {
+        currencies: {},
+        base: "RUB",
+        amount: "",
+        convertTo: "EUR",
+        result: "",
+    };
 
-    function callback1 (e) {
-        setSumInFromCurrency(false)
-        setFromCurrency(e.target.value)
-    }
 
-    function callback2 (e) {
-        setSumInFromCurrency(true)
-        setToCurrency(e.target.value)
-    }
+    handleSelect = e => {
+        this.setState(
+            {
+                [e.target.name]: e.target.value,
+                result: null
+            },
+            this.calculate
+        );
+    };
 
-    return (
-        <div className="App">
-            <h1>Currency Converter</h1>
-            <div className="content">
-                <Currency currencyName={currencyName}
-                          selectedCurrency={fromCurrency}
-                          onChangeCurrency={callback1}
-                          sum={fromSum}
-                          onChangeSum={handleFromSumChange}
-                />
-                <span className="equal">
-                    =
-                </span>
-                <Currency currencyName={currencyName}
-                          selectedCurrency={toCurrency}
-                          onChangeCurrency={callback2}
-                          sum={toSum}
-                          onChangeSum={handleToSumChange}
-                />
+    handleInput = e => {
+        this.setState(
+            {
+                amount: e.target.value,
+                result: null,
+            },
+            this.calculate
+        );
+    };
+
+    calculate = () => {
+        const amount = this.state.amount;
+        if (amount === isNaN) {
+            return;
+        } else {
+            const koef = this.state.currencies[this.state.base] / this.state.currencies[this.state.convertTo];
+            this.setState({
+                result: this.state.amount / koef
+            })
+        }
+    };
+
+
+    render() {
+        const {currencies, base, amount, convertTo, result} = this.state;
+        return (
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <h2>
+                                Converter
+                            </h2>
+                            <div>
+                                <div>
+                                    <form>
+                                        <input
+                                            type="number"
+                                            value={amount}
+                                            onChange={this.handleInput}
+                                        />
+                                        <select
+                                            name="base"
+                                            value={base}
+                                            onChange={this.handleSelect}
+                                        >
+                                            {Object.keys(currencies).map(currency => (
+                                                <option key={currency} value={currency}>
+                                                    {currency}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </form>
+
+                                    <form>
+                                        <input
+                                            disabled={true}
+                                            value={
+                                                amount === ""
+                                                    ? "0"
+                                                    : result === null
+                                                    ? "Calculating..."
+                                                    : result
+                                            }
+                                        />
+                                        <select
+                                            name="convertTo"
+                                            value={convertTo}
+                                            onChange={this.handleSelect}
+                                        >
+                                            {Object.keys(currencies).map(currency => (
+                                                <option key={currency} value={currency}>
+                                                    {currency}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default App;
+export default Converter;
+
